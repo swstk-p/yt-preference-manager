78c3831e20542fe6997ca8bcfb4c18b308cd96bc
Swastik Pandey <swastik.pandey@deerwalk.edu.np>
2024-11-11 20:41:16 +0545

    chore: check existing controller for a tab

--- extension/public/content-script.js
+++ extension/public/content-script.js
@@ -1,13 +1,5 @@
 console.log("IN PREFERENCE MANAGER");
 
-/**
- * Function to specify this extension while logging.
- * @param {*} msg message to log
- */
-function logThis(msg) {
-  console.log(`Ad Skipper: ${msg}`);
-}
-
 const xpaths = {
   skipBtn:
     "//button[(@class='ytp-skip-ad-button') and (contains(@id, 'skip-button'))]",
@@ -15,45 +7,27 @@ const xpaths = {
 };
 
 /**
- * Function to observe and click skip button
- * @param {*} node node representing the skip button
- */
-function observerAndClickBtn(node) {
-  //creating a mutation observer for node
-  const btnObserver = new MutationObserver((mutationsList) => {
-    //logging
-    console.log("Node is:", node);
-    console.log("Node style:", node.style);
-    console.log("Node length:", node.style.length);
-    console.log("Node display:", node.style.display);
-    // creating mouse click event for node
-    const clickEvent = new MouseEvent("click");
-    //dispatching mouse click event
-    const clicked = node.dispatchEvent(clickEvent);
-    console.log("Clicked:", clicked);
-    if (clicked) {
-      logThis("Skip button clicked");
-    } else {
-      logThis("Not clicked");
-    }
-  });
-  const btnObserverConfig = { attributes: true, attributeFilter: ["style"] };
-  btnObserver.observe(node, btnObserverConfig);
-}
-
-/**
  * Callback function for MutationObserver which checks if the skip button is present
  * @param {*} mutationsList parameter supplied by MutationObserver
  */
 function checkSkipBtn(mutationsList) {
-  chrome.runtime
-    .sendMessage({ event: "domChange", id: tab.id })
-    .then((res) => {
-      console.log(res);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+  const node = document.evaluate(
+    xpaths.skipBtn,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+  if (node !== null && node !== undefined) {
+    chrome.runtime
+      .sendMessage({ event: "skipAd" })
+      .then((res) => {
+        console.log("SkipAd res:", res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 }
 
 const domObserver = new MutationObserver(checkSkipBtn);
--- extension/src/service-worker.js
+++ extension/src/service-worker.js
@@ -1,21 +1,90 @@
-// const puppeteer = require("puppeteer-core/lib/esm/puppeteer/puppeteer-core-browser.js");
 import {
   connect,
   ExtensionTransport,
+  Page,
 } from "puppeteer-core/lib/esm/puppeteer/puppeteer-core-browser.js";
 
-console.log("Content script");
+let pageControllers = [];
+
+class PageController {
+  constructor(tabId) {
+    this.tabId = tabId; //to keep track of tabId
+    this.browser = null;
+    this.page = null;
+  }
+
+  /**
+   * Sets the browser and page objects as instance variables.
+   */
+  async init() {
+    try {
+      const browser = await connect({
+        transport: await ExtensionTransport.connectTab(this.tabId),
+      });
+      const [page] = await browser.pages();
+      this.browser = browser;
+      this.page = page;
+    } catch (err) {
+      console.log("Error during PageController init:", err);
+    }
+  }
+
+  /**
+   * Closes connection with the page.
+   */
+  async close() {
+    try {
+      await this.browser.disconnect();
+    } catch (err) {
+      console.log("Error while disconnecting the browser:", err);
+    }
+  }
+}
+
+/**
+ * Obtains PageController obj if it exists for given tab; else creates and returns a new one.
+ * @param {*} tabId ID number representing the tab.
+ * @returns A PageController obj
+ */
+async function getPageController(tabId) {
+  //finds and returns pageController
+  if (pageControllers.length > 0) {
+    console.log("Controller array length:", pageControllers.length); //logging
+    const pageController = pageControllers.find(
+      (controller) => controller.tabId === tabId
+    );
+    if (pageController !== undefined) {
+      console.log("pageController not undefined."); //logging
+      return pageController;
+    }
+  }
+  console.log("No pageController found"); //logging
+  //creates new pageController as none found
+  const pageController = new PageController(tabId);
+  await pageController.init();
+  pageControllers.push(pageController);
+  console.log("pageController pushed.");
+  return pageController;
+}
+
 chrome.runtime.onMessage.addListener(async (request, sender, sendResponse) => {
-  if (request.event === "domChange") {
-    // connecting page
-    const browser = await connect({
-      transport: await ExtensionTransport.connectTab(sender.tab.id),
-    });
-    const [page] = await browser.pages();
-    console.log("tab title:", await page.evaluate("document.title"));
-    browser.disconnect().then((res) => {
-      console.log("Page disconnected");
-    });
+  if (request.event === "skipAd") {
+    try {
+      //get controller obj
+      const pageController = await getPageController(sender.tab.id);
+      // close controller obj
+      await pageController.close();
+      console.log("Controller closed"); //logging
+      //removes controller obj from the controller array
+      pageControllers = pageControllers.filter(
+        (controller) => controller.tabId !== sender.tab.id
+      );
+      console.log("pageController removed"); //logging
+      sendResponse({ success: true });
+    } catch (err) {
+      console.log("Err while skipping ad:", err); //logging
+      sendResponse({ success: false });
+    }
     return true;
   }
 });
